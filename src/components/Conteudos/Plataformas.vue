814bb8b9c4d96b3362074289a2483366b4fd47b1
root <root@BETELNOTE557.beteltecnologia.net.br>
2022-09-27 03:45:16 -0300

    plataformas pronto

--- src/components/Conteudos/Plataformas.vue
+++ src/components/Conteudos/Plataformas.vue
@@ -39,7 +39,7 @@
             class="col-8 md:col-6 lg:col-3 text-center border-1 border-primary-700 border-round-2xl bg-blue-50 transition-colors transition-duration-1000 hover:bg-bluegray-100 shadow-5">
             <h3>Origin</h3>
             <div class="h-16rem">
-                <img class="imagem h-13rem" src="@/assets/imgs/plataformas/Origin.png"/>
+                <img class="imagem h-11rem" src="@/assets/imgs/plataformas/Origin.png"/>
             </div>
             <p-button label="Saiba mais..." icon="pi pi-info-circle" class="p-button-outlined"></p-button>
         </div>
